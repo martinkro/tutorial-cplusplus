cd597a170c0c0c674ab4cd8752b9b7833d397da5
chameleon <martinkro@aliyun.com>
2017-08-05 09:30:41 +0800

    CMake Demo

---
+++ CMakeTest2/main.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << test_add(1,2) << endl;
+    return 0;
+}
---
+++ CMakeTest2/src/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int test_add(int a, int b)
+{
+    return a + b;
+}
